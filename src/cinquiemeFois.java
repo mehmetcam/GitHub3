f212e3b3931cfeea50dd39648b326f0bd0fffcf6
mehmetcam <mehmetcam32@gmail.com>
2023-12-08 21:33:57 +0100

    4 5 fois

---
+++ src/cinquiemeFois.java
@@ -0,0 +1,5 @@
+public class cinquiemeFois {
+    public static void main(String[] args) {
+        System.out.println("c le 5");
+    }
+}
---
+++ src/quatriemeFois.java
@@ -0,0 +1,5 @@
+public class quatriemeFois {
+    public static void main(String[] args) {
+        System.out.println("naber");
+    }
+}
